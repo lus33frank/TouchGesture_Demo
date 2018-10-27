877be3ae73b69ac9cbb0a9857aa41ae6db07fcaa
lus33frank <lus33frank@gmail.com>
2018-10-27 18:23:33 +0800

    Commit : 2    專案練習完成。 觸摸手勢

--- app/src/main/java/com/frankchang/touchgesture_demo/MainActivity.java
+++ app/src/main/java/com/frankchang/touchgesture_demo/MainActivity.java
@@ -1,13 +1,62 @@
 package com.frankchang.touchgesture_demo;
 
+import android.annotation.SuppressLint;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView show;
+
+
+    @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        show = findViewById(R.id.tvShow);
+
+        ConstraintLayout view = findViewById(R.id.layout);
+        view.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                show.setText("");
+
+                int action = event.getAction();
+                int count = event.getPointerCount();
+
+                for (int i = 0; i < count; i++) {
+                    float x = event.getX();
+                    float y = event.getY();
+
+                    String msg = i + ".X: " + x + "; Y: " + y + "\n";
+                    show.setText(msg);
+                }
+
+                switch (action) {
+                    case MotionEvent.ACTION_DOWN:
+                        show.append("ACTION_DOWN");
+                        return true;
+
+                    case MotionEvent.ACTION_MOVE:
+                        show.append("ACTION_MOVE");
+                        return true;
+
+                    case MotionEvent.ACTION_UP:
+                        show.append("ACTION_UP");
+                        return true;
+
+                     default:
+                        return false;
+                }
+            }
+        });
     }
+
 }
